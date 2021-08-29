74b46429cfe6695d569bfc63afc524dae174f620
ABdevs29 <abhishek.elmoro@gmail.com>
2021-08-29 09:59:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+const curr_date = new Date();
+
+//API endpoint to create a text file in a particular folder
+app.get("/", (request, response) => {
+  fs.appendFile(
+    `./text_files/${
+      curr_date.getDate() +
+      "-" +
+      (curr_date.getMonth() + 1) +
+      "-" +
+      curr_date.getFullYear() +
+      "-" +
+      curr_date.getHours() +
+      ":" +
+      curr_date.getMinutes() +
+      ":" +
+      curr_date.getSeconds()
+    }.txt`,
+    `
+    ${Date()}`,
+    function (err) {
+      console.log(err);
+    }
+  );
+  response.send("Date file created. Please check!!");
+});
+
+//API endpoint to retrieve all the text files in text_files folder
+app.get("/files", async (request, response) => {
+  const file_names = [];
+  fs.readdirSync("./text_files").forEach((file) => {
+    file_names.push(file);
+  });
+
+  response.send(file_names);
+});
+
+app.listen(3800, () => console.log("Server has started"));
